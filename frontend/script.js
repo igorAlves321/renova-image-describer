8c6abb16558e1fc34e9b67c6c4f56792548fcc1a
DalisonCPU <dalisonmg@gmail.com>
2023-12-02 15:06:51 -0300

    Enviando frontend

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,13 @@
+//Criar áudio e carregar no buffer
+let musicLoop = new Audio
+musicLoop.src = "loop.mp3"
+musicLoop.loop = true
+musicLoop.volume = 0.3
+
 let btnAddImage = document.getElementById("btnAddFile")
 let contentImage = ""
+let dvResultado = document.getElementById("resultado")
+let fileImage = document.getElementById("fileImage")
 
 btnAddImage.addEventListener("click", imageEvents)
 
@@ -10,19 +18,45 @@ function imageEvents() {
     inptImage.click()
     inptImage.addEventListener("change", readImage, false)
 
-    let fileImage = document.getElementById("fileImage")
 
-    function readImage() {
+    async function readImage() {
         let fr = new FileReader()
 
-        fr.onload = function (event) {
+        fr.onload = async function (event) {
 
             fileImage.src = event.target.result
             contentImage = fileImage.src
-            navigator.clipboard.writeText(contentImage)
-            fileImage.removeAttribute("alt")
+            const json = {
+                image: contentImage
+            }
+            musicLoop.play()
+            dvResultado.innerHTML = "Processando..."
+            try {
+                const result = await fetch("http://localhost:3000", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(json)
+                })
+
+                let data = await result.json()
+                data = data.data
+                fileImage.setAttribute("alt", data)
+                dvResultado.innerHTML = ""
+            } catch (err) {
+                console.log("Erro ao obter informações do servidor")
+                dvResultado.innerHTML = "Erro ao obter informações do servidor."
+            }
+
+            musicLoop.pause()
+            musicLoop.currentTime = 0
 
         }
         fr.readAsDataURL(this.files[0])
     }
 }
+
+fileImage.addEventListener("click", (() => {
+    navigator.clipboard.writeText(fileImage.alt)
+}))
