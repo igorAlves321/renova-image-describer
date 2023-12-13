a05b4b89edace4ccc3797c986407247f73b79ce7
igorAlves321 <igoralvespalmeirag4@gmail.com>
2023-12-13 14:28:37 -0300

    crude feito parte do backend.

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -4,9 +4,30 @@ const prisma = new PrismaClient()
 
 class UserController {
     async create(req, res) {
-        return res.status(200).send({
-            message: "Hello world!"
+        const user = await prisma.user.create({
+            data: req.body,
         })
+        return res.status(201).json(user)
+    }
+
+    async read(req, res) {
+        const users = await prisma.user.findMany()
+        return res.status(200).json(users)
+    }
+
+    async update(req, res) {
+        const user = await prisma.user.update({
+            where: { id: req.params.id },
+            data: req.body,
+        })
+        return res.status(200).json(user)
+    }
+
+    async delete(req, res) {
+        const user = await prisma.user.delete({
+            where: { id: req.params.id },
+        })
+        return res.status(200).json(user)
     }
 }
 
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -5,5 +5,8 @@ const userController = new UserController()
 const router = express.Router()
 
 router.post("/create", userController.create)
+router.get("/read", userController.read)
+router.put("/update/:id", userController.update)
+router.delete("/delete/:id", userController.delete)
 
 export default router
